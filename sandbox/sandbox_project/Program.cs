536401f66b5cd0ad3a82209bde7e7845329321e4
BARAZAJ <barazajohn52@gmail.com>
2024-09-21 01:30:00 +0300

    code

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -4,10 +4,20 @@ public class Program
 {
     static void Main(string[] args)
     {
-        // This project is here for you to use as a "Sandbox" to play around
-        // with any code or ideas you have that do not directly apply to
-        // one of your projects.
+        
 
         Console.WriteLine("Hello Sandbox World!");
+        var numbers = new int[3];
+
+        numbers[0] =1;
+        numbers[1]=2;
+        numbers[2]=3;
+        
+
+
+
+
+
+
     }
 }
--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -12,8 +12,17 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        //STEP1: We first define the function and make sure it accepts two parameters - one for the number and the second for the length of the list of multiples of that number.
+        //STEP2: Create an array of doubles with the length from step 1
+        //STEP3: Use the "for" loop to fill the above array with multiples of the number. The first element in the set is the number, the second is number*2 and so forth until the length is reached.
+        //STEP4: Return the populated array
+        double [] multiples = new double[length];
+        for (int i=0; i <length; i++)
+        {
+            multiples[i] = number * (i+1);
+        }
 
-        return []; // replace this return statement with your own
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +38,32 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        //STEP 1: The first thing to do is define the RotateListRight function to two parameters, a list and the the amount the List should rotate
+        //STEP 2: Write a method to check if the amout is not less than 1 or greater than the number of elements in the list
+        //STEP 3: Apply modulo to create effective rotation. Modulo handles cases where the amount is greater than the data count.
+        //STEP 4: You break the list into two slices. I found it better to use GetRange to create the functions.
+        //STEP 5: Clear the original list
+        //STEP 6: Add the rotated lists by adding the last part first then the first.
+
+        if (amount < 1|| amount > data.Count)
+        {
+            throw new  ArgumentOutOfRangeException("amount", "Amount must be between 1 and the count of the data.");
+        }
+
+        int effectiveAmount = amount % data.Count;
+
+        var lastpart = data.GetRange(data.Count - effectiveAmount, effectiveAmount);
+        var firstpart = data.GetRange(0,data.Count- effectiveAmount);
+        
+        data.Clear();
+        data.AddRange(lastpart);
+        data.AddRange(firstpart);
+
+
     }
+
+
+
+
+
 }
